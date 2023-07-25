f71f8ea578fc5eaf981871d59b39524d091b92d6
owen0621 <owen10270936@gmail.com>
2023-07-26 00:51:41 +0800

    feat: can insert json to db via insert_json.py

--- database/scheme.sql
+++ database/scheme.sql
@@ -3,15 +3,15 @@ CREATE SCHEMA `HACKATHON`;
 USE `HACKATHON`;
 CREATE TABLE `court_cases` (
     `id` INT PRIMARY KEY AUTO_INCREMENT,
-    `court` VARCHAR(50),
-    `date` DATETIME,
-    `no` VARCHAR(50),
-    `sys` VARCHAR(50),
-    `reason` VARCHAR(255),
-    `type` VARCHAR(50),
-    `historyHash` VARCHAR(100),
-    `mainText` TEXT,
-    `opinion` TEXT,
-    `judgement` TEXT NOT NULL,
-    `summary` TEXT
+    `court` VARCHAR(50) NOT NULL,
+    `date` DATETIME NOT NULL,
+    `no` VARCHAR(50) NOT NULL,
+    `sys` VARCHAR(50) NOT NULL,
+    `reason` VARCHAR(255) NOT NULL,
+    `type` VARCHAR(50) NOT NULL,
+    `historyHash` VARCHAR(100) NOT NULL,
+    `mainText` LONGTEXT NOT NULL,
+    `opinion` LONGTEXT NOT NULL,
+    `judgement` LONGTEXT NOT NULL,
+    `summary` LONGTEXT DEFAULT NULL
 );
